42429c12c30b0d38f9ac8f37e57acf3042cd8148
Joonas Blomberg <maindi66@gmail.com>
2013-01-09 02:46:39 +0200

    Added static_pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Testsite::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ Testsite::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
