06cd3ae7a4b02282185ec8584d482bb3092177e6
Joonas Blomberg <maindi66@gmail.com>
2013-01-10 19:04:17 +0200

    jees

--- config/application.rb
+++ config/application.rb
@@ -51,8 +51,10 @@ module Testsite
     # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    config.assets.enabled = false
 	
+    config.assets.precompile += %w[ bootstrap/*.css bootstrap/*.js ]
+  
 	# Heroku requires this to be false
 	config.assets.initialize_on_precompile=false
     # Version of your assets, change this if you want to expire all your assets
