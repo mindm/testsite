9a752551881e8f47fa52fb747fb18c29cae25add
Joonas Blomberg <maindi66@gmail.com>
2013-01-10 18:48:14 +0200

    works

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Testsite::Application.routes.draw do
-  match '/home'     to: 'static_pages#home'
-  match '/help'     to: 'static_pages#help'
-  match '/about'    to: 'tatic_pages#about'
-  match '/contact'  to: 'static_pages#contact'
+  match '/home',     to: 'static_pages#home'
+  match '/help',     to: 'static_pages#help'
+  match '/about',    to: 'static_pages#about'
+  match '/contact',  to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
