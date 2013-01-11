d7fd1bf6e76a22c39797bb897572794c8b9d642b
Joonas Blomberg <maindi66@gmail.com>
2013-01-11 22:21:46 +0200

    signout signin working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      sign_in
+      sign_in user
       redirect_to user
     else 
       flash.now[:error] = 'Invalid email/password combination'
@@ -15,6 +15,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    sign_out
+    redirect_to root_url
   end
 
 end
