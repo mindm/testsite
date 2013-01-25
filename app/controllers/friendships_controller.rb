79d3db1bbd0d0b24cfa3a5ef1fb3fb2ed0e14739
Joonas Blomberg <maindi66@gmail.com>
2013-01-25 14:17:25 +0200

    friendship feed updated to ajax compitability

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,9 +1,12 @@
 class FriendshipsController < ApplicationController
   #before_filter
   
+  def show
+  end
+  
   def index
     @friends = current_user.friends
-    @pag_friends = current_user.friends.paginate(page: params[:page], per_page: 25)
+    @pag_friends = current_user.friends.paginate(page: params[:page], per_page: 3)
     @pending_invited_by = current_user.pending_invited_by
     @pending_invited = current_user.pending_invited
   end
@@ -19,10 +22,13 @@ class FriendshipsController < ApplicationController
   end
     
   def update
+    @update_param = params[:foo]
+    @pag_friends = current_user.friends.paginate(page: params[:page], per_page: 3)
+    @friends = current_user.friends
     @friend_id = params[:id]
     @Friend = User.find(@friend_id)
     @friendship_approved = current_user.approve(@Friend)
-    if params[:foo] == 'decline'
+    if @update_param == 'decline'
       #Can't do anything about this, this is how you decline
       current_user.remove_friendship(@Friend)
     end
@@ -34,13 +40,17 @@ class FriendshipsController < ApplicationController
   end
   
   def destroy
+    @friends = current_user.friends
+    @pag_friends = current_user.friends.paginate(page: params[:page], per_page: 3)
     @Friend = User.find(params[:id])
     @friendship = current_user.send(:find_any_friendship_with, @Friend)
     if @friendship
       @friendship.delete
       @removed = true
-      flash[:notice] = "Removed #{@Friend.name} from friends"
-      redirect_to friendships_url
+      respond_to do |format|
+        format.html {redirect_to friendships_url}
+        format.js
+      end
     end
   end
   
