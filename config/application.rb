3960cde03eefb5d016699ada6919213fa707e824
Joonas Blomberg <maindi66@gmail.com>
2013-02-04 23:33:57 +0200

    Updated fonts and fixed a problem with Sign up

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module Testsite
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
